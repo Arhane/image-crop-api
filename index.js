da96122ca4b00798ff78817cdc5b3b6aa8d86037
Andrey Lozhkin <las@htc-cs.ru>
2017-12-25 12:24:26 +0400

    Add cropping image

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const fs = require('fs');
+const gm = require('gm');
 const app = express();
 const port = 3000;
 
 app.get('/ping', (request, response) => {
     response.send('Pong!');
+});
+
+app.get('/crop', (request, response) => {
+
+    const { width, height } = request.query; // Get the width and height from the request parameters
+
+    gm(`${__dirname}/440639-1280x720.jpg`)
+        .crop(width, height)
+        .write(`${__dirname}/tmp.png`, (err) => {
+            if (err) {
+                console.log(err);
+                response.send(err);
+            } else {
+                response.sendFile(`${__dirname}/tmp.png`);
+            }
+        })
+
+    // fs.unlinkSync('./tmp.png'); // Delete the temporary file that we created in the cropping task
 });
 
 app.listen(port, () => {
