98684e75b90feb7425944b8d3d748221f5cc6620
Andrey Lozhkin <las@htc-cs.ru>
2017-12-25 17:31:56 +0400

    Partial commit

--- index.js
+++ index.js
@@ -18,22 +18,34 @@ app.get('/ping', (request, response) => {
 
 app.post('/crop', (request, response) => {
     const busboy = new Busboy({ headers: request.headers });
+    let temp = {};
     busboy.on('file', (fieldName, file, filename, encoding, mimetype) => {
         console.log('File [' + fieldName + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
         file.on('data', function(data) {
             console.log('File [' + fieldName + '] got ' + data.length + ' bytes');
         });
         file.on('end', function() {
+            temp[fieldName] = file;
             console.log('File [' + fieldName + '] Finished');
         });
     });
     busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
         console.log('Field [' + fieldname + ']: value: ' + val);
+        temp[fieldname] = val;
     });
     busboy.on('finish', function() {
         console.log('Done parsing form!');
-        response.writeHead(303, { Connection: 'close', Location: '/' });
-        response.end();
+        const { image, width, height } = temp;
+        gm(temp.image, 'img.png')
+            .crop(width, height)
+            .write(`${__dirname}/tmp.png`, (err) => {
+                if (err) {
+                    console.log(err);
+                    response.send(err);
+                } else {
+                    response.sendFile(`${__dirname}/tmp.png`);
+                }
+            });
     });
     request.pipe(busboy);
 });
